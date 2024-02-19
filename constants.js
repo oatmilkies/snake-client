8d1f04478bb48b71b9da66c267ba8bda3b3f8f85
Leilani <pquaby@gmail.com>
2024-02-19 11:17:14 -0500

    added constants file and moved code

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = "localhost";
+const PORT = 50541;
+
+const keyMappings = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  m: "Say: meow"
+}
+
+module.exports = {
+  IP,
+  PORT,
+  keyMappings
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { keyMappings } = require("./constants");
+
 // Stores the active TCP connection object.
 let connection;
 
@@ -16,17 +18,9 @@ const setupInput = function(conn) {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  } else if (key === "w") {
-    connection.write("Move: up");
-  } else if (key === "a") {
-    connection.write("Move: left");
-  } else if (key === "s") {
-    connection.write("Move: down");
-  } else if (key === "d") {
-    connection.write("Move: right");
-  } else if (key === "m") {
-    connection.write("Say: meow");
-  }
+  } else
+    connection.write(keyMappings[key]);
+
 };
 
 module.exports = {
