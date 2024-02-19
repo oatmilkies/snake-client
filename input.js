aa78e0269f5758cc50b448d167d3269ec37aa85e
Leilani <pquaby@gmail.com>
2024-02-19 11:01:35 -0500

    added connection variable

--- input.js
+++ input.js
@@ -1,5 +1,10 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,10 +13,18 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-  };
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = {
